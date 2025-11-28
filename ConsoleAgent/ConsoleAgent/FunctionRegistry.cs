5302a4b3d41ba9599e85ed6477578b43e13b6f53
Francisco Arturo Licon Colon <franciscoliconcolon@gmail.com>
2025-11-27 22:35:24 -0600

    Deleting mock services

--- ConsoleAgent/ConsoleAgent/FunctionRegistry.cs
+++ ConsoleAgent/ConsoleAgent/FunctionRegistry.cs
@@ -20,26 +20,6 @@ public static class FunctionRegistry
                 Description = "Get the current weather descriptions in a specified city"
             });
 
-        WardrobeService wardrobeService = serviceProvider.GetRequiredService<WardrobeService>();
-        MethodInfo getWardrobeFunction = typeof(WardrobeService)
-            .GetMethod(nameof(WardrobeService.ListClothing), [])!;
-
-        yield return AIFunctionFactory.Create(getWardrobeFunction, wardrobeService,
-            new AIFunctionFactoryOptions
-            {
-                Name = "get_clothing_from_wardrobe",
-                Description = "List all the clothing I have in my wardrobe"
-            });
-
-        EmailService emailService = serviceProvider.GetRequiredService<EmailService>();
-        MethodInfo getEmailFunction = typeof(EmailService)
-            .GetMethod(nameof(EmailService.EmailFriend), [typeof(string), typeof(string)])!;
-
-        yield return AIFunctionFactory.Create(getEmailFunction, emailService,
-            new AIFunctionFactoryOptions
-            {
-                Name = "email_friend",
-                Description = "Sends an email to my friend with this name"
-            });
+       
     }
 }
